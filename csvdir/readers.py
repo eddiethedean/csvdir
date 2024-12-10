4621d2ac28fa887d229de859efd63aff4c60e7bf
Odos Matthews <odosmatthews@Odoss-Mac-mini.local>
2024-12-10 14:11:17 -0500

    add dilimeter option

--- csvdir/readers.py
+++ csvdir/readers.py
@@ -9,15 +9,16 @@ import typing
 class IterPathsCsvDir:
     path: str
     extension: str = 'csv'
+    delimiter: str = ','
  
     def __post_init__(self) -> None:
-        self.csv_file_paths = glob.glob(os.path.join(self.path, f'*.{self.extension}'))
+        self.csv_file_paths = get_csv_paths(self.path, self.extension)
         self.reader = self.dict_rows()
        
     def dict_rows(self) -> typing.Generator[tuple[str, dict], None, None]:
         for path in self.csv_file_paths:
             with open(path, 'r') as f:
-                reader = csv.DictReader(f)
+                reader = csv.DictReader(f, delimiter=self.delimiter)
                 for row in reader:
                     yield path, row
            
@@ -31,7 +32,7 @@ class IterPathsCsvDir:
 class IterNamesCsvDir(IterPathsCsvDir):
     def dict_rows(self) -> typing.Generator[tuple[str, dict], None, None]:
         for full_path, row in IterPathsCsvDir.dict_rows(self):
-            yield os.path.basename(full_path), row
+            yield get_name(full_path), row
  
  
 class IterCsvDir(IterPathsCsvDir):
@@ -47,6 +48,7 @@ class IterCsvDir(IterPathsCsvDir):
 class CsvDir:
     path: str | None = None
     extension: str = 'csv'
+    delimiter: str = ','
  
     def __post_init__(self) -> None:
         if self.path is None:
@@ -54,24 +56,33 @@ class CsvDir:
     
     @property
     def paths(self):
-        return glob.glob(os.path.join(self.path, f'*.{self.extension}'))
+        return get_csv_paths(self.path, self.extension)
     
     @property
     def names(self):
-        return [os.path.basename(path) for path in self.paths]
+        return [get_name(path) for path in self.paths]
 
     def __iter__(self) -> IterCsvDir:
-        return IterCsvDir(self.path, extension=self.extension)
+        return IterCsvDir(self.path, extension=self.extension, delimiter=self.delimiter)
  
     def with_names(self) -> IterNamesCsvDir:
-        return IterNamesCsvDir(self.path, extension=self.extension)
+        return IterNamesCsvDir(self.path, extension=self.extension, delimiter=self.delimiter)
  
     def with_paths(self) -> IterNamesCsvDir:
-        return IterPathsCsvDir(self.path, extension=self.extension)
+        return IterPathsCsvDir(self.path, extension=self.extension, delimiter=self.delimiter)
     
 
 def read_dir(
     path: str | None = None,
-    extension: str = 'csv'
+    extension: str = 'csv',
+    delimiter: str = ','
 ) -> CsvDir:
-    return CsvDir(path, extension)
+    return CsvDir(path, extension, delimiter)
+
+
+def get_name(path: str) -> str:
+    return os.path.splitext(os.path.basename(path))[0]
+
+
+def get_csv_paths(path: str, extension: str) -> list[str]:
+    return glob.glob(os.path.join(path, f'*.{extension}'))
