185d22e23836f39a791eaa1ab160d1e8e0b651ed
Odos Matthews <odosmatthews@Odoss-Mac-mini.local>
2024-12-11 08:44:36 -0500

    add enumerate options

--- csvdir/readers.py
+++ csvdir/readers.py
@@ -6,7 +6,7 @@ import typing
  
  
 @dataclass
-class IterPathsCsvDir:
+class IterEnumPathsCsvDir:
     path: str
     extension: str = 'csv'
     delimiter: str = ','
@@ -15,26 +15,60 @@ class IterPathsCsvDir:
         self.csv_file_paths = get_csv_paths(self.path, self.extension)
         self.reader = self.dict_rows()
        
-    def dict_rows(self) -> typing.Generator[tuple[str, dict], None, None]:
+    def dict_rows(self) -> typing.Generator[tuple[int, str, dict], None, None]:
         for path in self.csv_file_paths:
             with open(path, 'r') as f:
                 reader = csv.DictReader(f, delimiter=self.delimiter)
-                for row in reader:
-                    yield path, row
+                for i, row in enumerate(reader):
+                    yield i, path, row
+           
+    def __next__(self) -> tuple[int, str, dict]:
+        return next(self.reader)
+ 
+    def __iter__(self):
+        return self
+    
+
+@dataclass
+class IterPathsCsvDir(IterEnumPathsCsvDir):
+    def dict_rows(self) -> typing.Generator[tuple[str, dict], None, None]:
+        for _, full_path, row in IterEnumPathsCsvDir.dict_rows(self):
+            yield full_path, row
            
     def __next__(self) -> tuple[str, dict]:
         return next(self.reader)
  
     def __iter__(self):
         return self
+    
+    def enumerate(self) -> IterEnumPathsCsvDir:
+        return IterEnumPathsCsvDir(self.path, self.extension, self.delimiter)
+    
  
- 
+class IterEnumNamesCsvDir(IterEnumPathsCsvDir):
+    def dict_rows(self) -> typing.Generator[tuple[int, str, dict], None, None]:
+        for i, full_path, row in IterEnumPathsCsvDir.dict_rows(self):
+            yield i, get_name(full_path), row
+
+
 class IterNamesCsvDir(IterPathsCsvDir):
     def dict_rows(self) -> typing.Generator[tuple[str, dict], None, None]:
         for full_path, row in IterPathsCsvDir.dict_rows(self):
             yield get_name(full_path), row
+
+    def enumerate(self):
+        return IterEnumNamesCsvDir(self.path, self.extension, self.delimiter)
  
  
+class IterEnumCsvDir(IterEnumPathsCsvDir):
+    def dict_rows(self) -> typing.Generator[tuple[int, dict], None, None]:
+        for i, _, row in IterEnumPathsCsvDir.dict_rows(self):
+            yield i, row
+           
+    def __next__(self) -> tuple[int, dict]:
+        return next(self.reader)
+ 
+
 class IterCsvDir(IterPathsCsvDir):
     def dict_rows(self) -> typing.Generator[dict, None, None]:
         for _, row in IterPathsCsvDir.dict_rows(self):
@@ -42,7 +76,10 @@ class IterCsvDir(IterPathsCsvDir):
            
     def __next__(self) -> dict:
         return next(self.reader)
- 
+    
+    def enumerate(self) -> IterEnumCsvDir:
+        return IterEnumCsvDir(self.path, self.extension, self.delimiter)
+    
  
 @dataclass
 class CsvDir:
@@ -70,6 +107,9 @@ class CsvDir:
  
     def with_paths(self) -> IterNamesCsvDir:
         return IterPathsCsvDir(self.path, extension=self.extension, delimiter=self.delimiter)
+    
+    def enumerate(self) -> IterEnumCsvDir:
+        return IterEnumCsvDir(self.path, extension=self.extension, delimiter=self.delimiter)
     
 
 def read_dir(
